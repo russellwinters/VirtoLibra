1ec1944db04592064b1988ac4dcf2e9acef84bb5
William L <w-lindvall@outlook.com>
2020-03-26 14:59:00 -0700

    delete express server

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-
-app.use(cors());
-
-app.listen(5000, () => {
-  console.log("listening on port 5000");
-});
