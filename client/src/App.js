8fd69d9ea7cb6a80e8fd030b5f59f40e48305bcc
William L <w-lindvall@outlook.com>
2020-03-26 17:54:42 -0700

    add example of custom hooks in use

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Components/Header";
 import {FirebaseProvider} from "./context";
+import FirebaseExample from "./Components/FirebaseExample";
 
 function App() {
   return (
     <FirebaseProvider>
       <div className="App">
         <Header />
+        <FirebaseExample />
       </div>
     </FirebaseProvider>
   );
