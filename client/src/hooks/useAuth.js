4070787ad942cc5d8d44ea03b621a0c8f556eb0d
William L <w-lindvall@outlook.com>
2020-03-28 23:11:24 -0700

    create custom auth hook

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,42 @@
+import {useContext, useState} from "react";
+import {FirebaseContext} from "../context/firebase";
+
+export const useAuth = () => {
+  const {auth} = useContext(FirebaseContext);
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState();
+  const [error, setError] = useState();
+
+  const signUpWithEmailAndPassword = (email, password) => {
+    setLoading(true);
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(({user}) => setUser(user))
+      .catch(setError)
+      .finally(() => setLoading(false));
+  };
+
+  const signInWithEmailAndPassword = (email, password) => {
+    setLoading(true);
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(({user}) => setUser(user))
+      .catch(setError)
+      .finally(() => setLoading(false));
+  };
+
+  const logOut = () => {
+    auth.signOut();
+    setUser(null);
+  };
+
+  return {
+    user,
+    error,
+    loading,
+    signUp: signUpWithEmailAndPassword,
+    signIn: signInWithEmailAndPassword,
+    logOut,
+  };
+};
+
