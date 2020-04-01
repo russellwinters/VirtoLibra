cf51c3a49b2b6356c994e0eafcd5a1b83c8d731c
Russell Winters <russellwinters@protonmail.com>
2020-04-01 11:19:36 -0700

    Post request working from the server side. Can now add posts to JSON files

--- routes/feeds.js
+++ routes/feeds.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
 router.get("/:genre", (req, res) => {
   console.log(req.params);
@@ -8,6 +9,29 @@ router.get("/:genre", (req, res) => {
   console.log(files[0]);
   res.json({
     data: files[0]
+  });
+});
+
+router.post("/:genre", (req, res) => {
+  console.log(req.body);
+  let path = `../models/${req.params.genre}.json`;
+  let files = require(path);
+  let feed = files[0].feed;
+  let newFeedItem = req.body;
+  feed.push(newFeedItem);
+  console.log(feed);
+
+  let JSON_File = `${__dirname}/../models/${req.params.genre}.json`;
+  let fullFile = [
+    {
+      genre: req.params.genre,
+      feed: feed
+    }
+  ];
+  fs.writeFileSync(JSON_File, JSON.stringify(fullFile));
+  console.log(JSON_File);
+  res.json({
+    message: "success"
   });
 });
 
