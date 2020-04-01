5c88e42c78ecdd1f357743bb61f0cafbb6cb0071
Russell Winters <russellwinters@protonmail.com>
2020-04-01 16:41:21 -0700

    Deploying to Heroku

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ app.use(cors());
 
 app.use("/api/feed", require("./routes/feeds"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(5000, () => {
   console.log("Listening on port 5000");
 });
